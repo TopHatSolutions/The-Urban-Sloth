52ecd3bd7325431fffca06edd196658b4f0d6210
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-14 17:34:56 -0600

    added permit params for products

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
-
+  :name, :description, :price, :stock_quantity, :brand_id, :category_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
   has_many :product_options
-  has_many :options, :through => :options
+  has_many :options, :through => :product_options
   has_many :lifestyle_products
   has_many :lifestyles, :through => :lifestyle_products
 end
