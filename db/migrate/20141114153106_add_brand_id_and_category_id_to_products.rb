3c59ec755d76e2e940a3124c30428b4f389b2e7a
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-14 09:32:28 -0600

    added db migration for products associations

---
+++ db/migrate/20141114153106_add_brand_id_and_category_id_to_products.rb
@@ -0,0 +1,6 @@
+class AddBrandIdAndCategoryIdToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :brand_id, :integer
+    add_column :products, :category_id, :integer
+  end
+end
