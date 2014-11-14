a421e4400608ee470528615c418c77ad9200424e
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-14 17:18:11 -0600

    Added permit params for options

--- app/admin/option.rb
+++ app/admin/option.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Option do
-
+  permit_params :option_type, :option_value, :option_price
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
