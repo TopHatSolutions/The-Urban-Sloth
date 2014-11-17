844d9f592f6634b7d1a7493ad4fd06334e7e078e
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-17 12:11:40 -0600

    ran migration for pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117175937) do
+ActiveRecord::Schema.define(version: 20141117180923) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -110,6 +110,13 @@ ActiveRecord::Schema.define(version: 20141117175937) do
     t.decimal  "total"
     t.decimal  "sub_total"
     t.integer  "customer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pages", force: true do |t|
+    t.string   "name"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
