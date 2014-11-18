fa345ea181b93ec5661bbe62af33936800d880e5
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-17 18:10:55 -0600

    added permit params for both the admin controller and product controller

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :price, :stock_quantity, :brand_id, :category_id
+  permit_params :name, :description, :price, :stock_quantity, :brand_id, :category_id, :image
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
