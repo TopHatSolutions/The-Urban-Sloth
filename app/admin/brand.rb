3c535ec88401bb64cec43ed3e2eacd5199c7cb1a
Andrew Gillespie <gillespie.andrewc@gmail.com>
2014-11-19 18:13:53 -0600

    added permit params for brands

--- app/admin/brand.rb
+++ app/admin/brand.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Brand do
-  permit_params :name
+  permit_params :name, :category_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
